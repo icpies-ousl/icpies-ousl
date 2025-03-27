73d0399aa54dfc7010f815246f8eeeeace467ae9
mayura-andrew <mayuraalahakoon@gmail.com>
2025-03-27 12:26:28 +0530

    Rename project to icpies and update site URL; add deployment workflow and script

--- site.config.ts
+++ site.config.ts
@@ -1,4 +1,4 @@
-export const siteUrl = "https://mizar.majestico.co";
+export const siteUrl = "https://icpies.ou.ac.lk";
 export const siteTitle = "Mizar";
 export const defaultLocale = "en";
 export const locales = ["en"];
